a1658f9d23b6de4aee06374714efaccffcc01114
sciboy12 <48741660+sciboy12@users.noreply.github.com>
2019-03-31 18:11:44 -0400

    Update Abs.py

--- Abs.py
+++ Abs.py
@@ -1,9 +1,9 @@
 from numpy import interp
 from Xlib import X, display
-from fractions import Fraction
 import evdev
-import subprocess
-import sys
+from config import Config
+#import yaml
+
 
 # Config - FILL THESE IN
 touchpadid='/dev/input/event5'
@@ -15,23 +15,6 @@ touchpad_y_min = 1062
 touchpad_y_max = 4690
 # End of config
 
-# Get display resolution
-resolution = display.Display().screen().root.get_geometry()
-display_resolution_x = resolution.width
-display_resolution_y = resolution.height
-
-# WIP sensitivity scaler
-#sensitivity = 100 # In percent
-#touchpad_x_min_scaled = sensitivity * (touchpad_x_min / 100)
-#touchpad_x_max_scaled = touchpad_x_max - touchpad_x_min_scaled
-#touchpad_y_min_scaled = 1 - (sensitivity * touchpad_y_min) / 100
-#touchpad_y_max_scaled = 1 - (sensitivity * touchpad_y_max) / 100 *6
-
-# WIP Ratio calculation algorithm
-#touchpad_x_total = touchpad_x_min - touchpad_x_max
-#touchpad_y_total = touchpad_y_min - touchpad_y_max
-#ratio_dec = touchpad_y_total / touchpad_x_total
-
 # Misc stuff to help speed up the loop
 d = display.Display()
 s = d.screen()
@@ -42,17 +25,49 @@ bytype = evdev.ecodes.bytype
 EV_ABS = evdev.events.EV_ABS
 warp_pointer = root.warp_pointer
 self = touchpadid
+sync = d.sync
+
+# WIP Config file handling
+#configfile = open('config.cfg', 'w')
+#configfile = file('config.cfg')
+#config = Config(configfile)
+#config.save(configfile)
+
+# Get display resolution
+resolution = root.get_geometry()
+display_resolution_x = resolution.width
+display_resolution_y = resolution.height
+
+# WIP sensitivity scaler
+#sensitivity = 100 # In percent
+#touchpad_x_min_scaled = sensitivity * (touchpad_x_min / 100)
+#touchpad_x_max_scaled = touchpad_x_max - touchpad_x_min_scaled
+#touchpad_y_min_scaled = 1 - (sensitivity * touchpad_y_min) / 100
+#touchpad_y_max_scaled = 1 - (sensitivity * touchpad_y_max) / 100 *6
+
+# Ratio calculation algorithm
+touchpad_x_total = touchpad_x_min - touchpad_x_max
+touchpad_y_total = touchpad_y_min - touchpad_y_max
+ratio_dec = touchpad_y_total / touchpad_x_total
+
+# Partially working, WIP - Scale the Y axis to compensate for ratio differences
+touchpad_y_min_scaled = touchpad_y_min * (ratio_dec * 2) # * (ratio_dec)
+touchpad_y_max_scaled = touchpad_y_max # * (ratio_dec)
+
+
 
 # Debug stuff
 #print('Touchpad X Resolution =',str(touchpad_x_min) + ',' + str(touchpad_x_max))
 #print('Touchpad Y Resolution =',str(touchpad_y_min) + ',' + str(touchpad_y_max))
 #print('Touchpad X Resolution(Scaled) =',str(touchpad_x_min_scaled) + ',' + str(touchpad_x_max_scaled))
 #print('Touchpad Y Resolution(Scaled) =',str(touchpad_y_min_scaled) + ',' + str(touchpad_y_max_scaled))
+#eventtype = read_loop.event.type
 
 print('Press Ctrl-C to quit.')
+
 try:
     for event in read_loop():
-        
+
         #Check for ABS_X events
         if event.type == EV_ABS and bytype[event.type][event.code] == 'ABS_X':
 
@@ -63,12 +78,13 @@ try:
         if event.type == EV_ABS and bytype[event.type][event.code] == 'ABS_Y':
             
             # Map the Touchpad Y limits to the screen resolution
-            interp_y = int(interp(event.value,[touchpad_y_min,touchpad_y_max],[0,display_resolution_y]))
+            interp_y = int(interp(event.value,[touchpad_y_min_scaled,touchpad_y_max_scaled],[0,display_resolution_y]))
             
             # Set the cursor position
             warp_pointer(interp_x,interp_y)
+
             # Sync the changes
-            d.sync()
+            sync()
             
 # Exit when Ctrl-C is pressed
 except KeyboardInterrupt:    
